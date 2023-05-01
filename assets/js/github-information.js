400ae2d657e9a6ee1b90ae09876e93a7ed86e1f8
Zaccco <samuelcollskog98@gmail.com>
2023-05-01 00:10:14 +0000

    Fetch repo information to users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -15,6 +15,26 @@ function userInformationHTML(user) {
             </div>`;
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    const listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
 
 
 function fetchGitHubInformation(event) {
@@ -28,13 +48,17 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
-    
+
+    // Outdated version of when and then I think
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            const userData = response;
+        function(firstResponse, secondResponse) {
+            const userData = firstResponse[0];
+            const repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for the user ${username}</h2>`);
